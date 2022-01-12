b9a3485147dc4652c710289904750ed8efd47da8
GulSam00 <gulsamcono@gmail.com>
2022-01-12 15:41:03 +0900

    Modfiy : 디자인 수정. 모달 디자인 적용 실패

--- src/Pages/MainPage/MainPage.styled.js
+++ src/Pages/MainPage/MainPage.styled.js
@@ -5,30 +5,11 @@ const MenuModal = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: ;translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+
   width: 400;
   bgcolor: background.paper;
   border: 2px solid #000;
-  width: 100%;
-  height : 100%;
-
-  // .alert-enter {
-  //   opacity: 0;
-  //   transform: scale(0.9);
-  // }
-  // .alert-enter-active {
-  //   opacity: 1;
-  //   transform: translateX(0);
-  //   transition: opacity 300ms, transform 300ms;
-  // }
-  // .alert-exit {
-  //   opacity: 1;
-  // }
-  // .alert-exit-active {
-  //   opacity: 0;
-  //   transform: scale(0.9);
-  //   transition: opacity 300ms, transform 300ms;
-  // }
 `;
 const MainHeader = styled.div`
   background-color: rgba(42, 45, 56, 1);
