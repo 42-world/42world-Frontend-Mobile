0ae62f19bca5c555346e912b881d88899df6da65
GulSam00 <gulsamcono@gmail.com>
2022-01-12 22:39:15 +0900

    Feat : @mui/icons-material 추가. 아이콘 변경.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  const navi = useNavigate();
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
-    console.log(search);
+    console.log(search); // 검색 창으로 이동해야 함.
+    navi(`/search?keyword=${search}`);
     setSearch("");
   };
   const handleChangeSearch = (e) => {
@@ -25,13 +33,22 @@ const SearchBar = () => {
     </form>
   </div>;
 };
-const Header = ({ handleOpenMenu }) => {
+
+const Header = ({ handleOpenMenu, isSearch }) => {
+  const navi = useNavigate();
+
   return (
     <div className="mainpage-header">
-      <img src="assets/menu.svg" onClick={handleOpenMenu} alt="menu" />
+      <MenuIcon sx={{ color: "white" }} onClick={handleOpenMenu} />
+      <SearchIcon sx={{ color: "white" }} onClick={() => navi("/search")} />
       <img src="assets/headerLogo.svg" alt="header-logo" />
-      <img src="assets/alarmNo.svg" alt="alarm" />
-      <img src="assets/myPage.svg" alt="my-page" />
+      <NotificationsIcon sx={{ color: "white" }} />
+      <AccountCircleIcon
+        sx={{ color: "white" }}
+        onClick={() => navi("/profile")}
+      />
+
+      {isSearch && <SearchBar />}
     </div>
   );
 };
