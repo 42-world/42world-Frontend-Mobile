d92ca60c6ce3b4a50989bf7a03f133834ab926c4
GulSam00 <gulsamcono@gmail.com>
2022-01-14 11:59:05 +0900

    Modify : 헤더 컴포넌트로 state, 함수 이전.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,32 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
+
+import MenuModal from './MenuModal';
+import NotiModal from './NotiModal';
+
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 import Styled from './Header.styled';
 
 const SearchBar = () => {
   const [search, setSearch] = useState('');
   const navi = useNavigate();
+
   const handleSubmitSearch = e => {
     e.preventDefault();
     console.log(search); // 검색 창으로 이동해야 함.
     navi(`/search?keyword=${search}`);
     setSearch('');
   };
+
   const handleChangeSearch = e => {
     setSearch(e.currentTarget.value);
   };
+
   return (
     <div>
       <form onSubmit={handleSubmitSearch}>
@@ -35,13 +43,51 @@ const SearchBar = () => {
   );
 };
 
-const Header = ({ onClickMenu, onClickNoti, onClickSearch, isSearch }) => {
+const Header = () => {
+  const [isMenuModal, setIsMenuModal] = useState(false);
+  const [isNotiModal, setIsNotiModal] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const loca = useLocation();
+
+  const handleOpenMenu = (anchor, open) => {
+    if (loca.search === '') {
+      setSearchParams('mode=menu');
+      setIsMenuModal(true);
+    } else {
+      setSearchParams('');
+      setIsMenuModal(false);
+    }
+  };
+  const handleOpenNoti = () => {
+    if (loca.search === '') {
+      setSearchParams('mode=noti');
+      setIsNotiModal(true);
+    } else {
+      setSearchParams('');
+      setIsNotiModal(false);
+    }
+  };
+  const handleToggleSearch = () => {
+    if (isSearch) setIsSearch(false);
+    else setIsSearch(true);
+  };
+  useEffect(() => {
+    if (loca.search === '?mode=menu') {
+      setIsMenuModal(true);
+    }
+    if (loca.search === '?mode=noti') {
+      setIsNotiModal(true);
+    }
+  }, []);
+
   const navi = useNavigate();
+
   return (
     <div className="mainpage-header">
       <Styled.HeaderStyleDiv>
         <div>
-          <MenuIcon sx={{ color: 'white' }} onClick={onClickMenu} />
+          <MenuIcon sx={{ color: 'white' }} onClick={handleOpenMenu} />
         </div>
         <div>
           <img
@@ -50,17 +96,31 @@ const Header = ({ onClickMenu, onClickNoti, onClickSearch, isSearch }) => {
             onClick={() => navi('/')}
           />
         </div>
-        {/* <SearchIcon sx={{ color: 'white' }} onClick={() => navi('/search')} /> */}
         <div>
-          <SearchIcon sx={{ color: 'white' }} onClick={onClickSearch} />
-          <NotificationsIcon sx={{ color: 'white' }} onClick={onClickNoti} />
+          <SearchIcon sx={{ color: 'white' }} onClick={handleToggleSearch} />
+          <NotificationsIcon sx={{ color: 'white' }} onClick={handleOpenNoti} />
           <AccountCircleIcon
             sx={{ color: 'white' }}
             onClick={() => navi('/profile')}
           />
         </div>
       </Styled.HeaderStyleDiv>
+
+      {/* <SearchIcon sx={{ color: 'white' }} onClick={() => navi('/search')} /> */}
       {isSearch && <SearchBar />}
+
+      <SwipeableDrawer
+        anchor="left"
+        open={isMenuModal}
+        onClose={handleOpenMenu}
+        onOpen={handleOpenMenu}
+      >
+        <MenuModal open={isMenuModal} onClickCloseModal={handleOpenMenu} />
+      </SwipeableDrawer>
+      {/*<Styled.MenuModal>*/}
+      {/*<MenuModal open={isMenuModal} onClickCloseModal={handleOpenMenu} />*/}
+      {/*<NotiModal open={isNotiModal} onClickCloseModal={handleOpenNoti} />*/}
+      {/*</Styled.MenuModal>*/}
     </div>
   );
 };
--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,72 +1,13 @@
-import { useState, useEffect } from 'react';
-import { useSearchParams, useLocation } from 'react-router-dom';
 import { Body } from './Components';
-import { Header, MenuModal, NotiModal } from '../../Components';
-
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import { Header } from '../../Components';
 
 import Styled from './MainPage.styled';
 
 const MainPage = () => {
-  const [isMenuModal, setIsMenuModal] = useState(false);
-  const [isNotiModal, setIsNotiModal] = useState(false);
-  const [isSearch, setIsSearch] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const loca = useLocation();
-
-  const handleOpenMenu = (anchor, open) => {
-    if (loca.search === '') {
-      setSearchParams('mode=menu');
-      setIsMenuModal(true);
-    } else {
-      setSearchParams('');
-      setIsMenuModal(false);
-    }
-  };
-  const handleOpenNoti = () => {
-    if (loca.search === '') {
-      setSearchParams('mode=noti');
-      setIsNotiModal(true);
-    } else {
-      setSearchParams('');
-      setIsNotiModal(false);
-    }
-  };
-  const handleToggleSearch = () => {
-    if (isSearch) setIsSearch(false);
-    else setIsSearch(true);
-  };
-  useEffect(() => {
-    if (loca.search === '?mode=menu') {
-      setIsMenuModal(true);
-    }
-    if (loca.search === '?mode=noti') {
-      setIsNotiModal(true);
-    }
-  }, []);
-
   return (
     <>
-      {/*<Styled.MenuModal>*/}
-      {/*<MenuModal open={isMenuModal} onClickCloseModal={handleOpenMenu} />*/}
-      {/*<NotiModal open={isNotiModal} onClickCloseModal={handleOpenNoti} />*/}
-      {/*</Styled.MenuModal>*/}
-      <SwipeableDrawer
-        anchor="left"
-        open={isMenuModal}
-        onClose={handleOpenMenu}
-        onOpen={handleOpenMenu}
-      >
-        <MenuModal open={isMenuModal} onClickCloseModal={handleOpenMenu} />
-      </SwipeableDrawer>
-
       <Styled.MainHeader>
-        <Header
-          onClickMenu={handleOpenMenu}
-          onClickNoti={handleOpenNoti}
-          onClickSearch={handleToggleSearch}
-          isSearch={isSearch}
-        />
+        <Header />
       </Styled.MainHeader>
       <Styled.MainBody>
         <Body />
