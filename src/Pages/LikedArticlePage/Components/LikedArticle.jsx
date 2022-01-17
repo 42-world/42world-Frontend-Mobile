0a3743bc423273996b396a38e6d6638299794474
PIut0 <slimdoks@gmail.com>
2022-01-17 20:20:09 +0900

    Feat: 좋아요 한 글 스타일

--- src/Pages/LikedArticlePage/Components/LikedArticle.jsx
+++ src/Pages/LikedArticlePage/Components/LikedArticle.jsx
@@ -1,12 +1,44 @@
+import { FavoriteBorder, SmsOutlined } from '@mui/icons-material';
+
+import Styled from './LikedArticle.styled';
+
 const LikedArticle = () => {
-  const articles = ["글1", "글2", "글3"];
+  const articles = [
+    {
+      board: '자유게시판',
+      title: '글제목1',
+      liked_count: '1',
+      comment_count: '1',
+    },
+    {
+      board: '자유게시판',
+      title: '글제목2',
+      liked_count: '2',
+      comment_count: '2',
+    },
+    {
+      board: '익명게시판',
+      title: '글제목3',
+      liked_count: '3',
+      comment_count: '3',
+    },
+  ];
   return (
     <>
-      <div>
+      <Styled.LikedArticlesDiv>
         {articles.map((article, idx) => (
-          <div key={idx}>{article}</div>
+          <Styled.LikedArticleDiv key={idx} article={article}>
+            <span className="article_board">{article.board}</span>
+            <span className="article_title">{article.title}</span>
+            <div className="favorite_icon">
+              <FavoriteBorder />
+            </div>
+            <div className="comment_icon">
+              <SmsOutlined />
+            </div>
+          </Styled.LikedArticleDiv>
         ))}
-      </div>
+      </Styled.LikedArticlesDiv>
     </>
   );
 };
