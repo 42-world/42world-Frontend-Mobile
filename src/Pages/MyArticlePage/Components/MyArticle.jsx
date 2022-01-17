2761bc246235df9fc01c34ffbea2c6fed4feb663
PIut0 <slimdoks@gmail.com>
2022-01-17 20:20:41 +0900

    Feat: 내가 쓴 글 스타일

--- src/Pages/MyArticlePage/Components/MyArticle.jsx
+++ src/Pages/MyArticlePage/Components/MyArticle.jsx
@@ -1,12 +1,44 @@
+import { FavoriteBorder, SmsOutlined } from '@mui/icons-material';
+
+import Styled from './MyArticle.styled';
+
 const MyArticle = () => {
-  const articles = ["글1", "글2", "글3"];
+  const articles = [
+    {
+      board: '자유게시판',
+      title: '내가쓴 글 글제목1',
+      liked_count: '1',
+      comment_count: '1',
+    },
+    {
+      board: '자유게시판',
+      title: '내가쓴 글 글제목2',
+      liked_count: '2',
+      comment_count: '2',
+    },
+    {
+      board: '익명게시판',
+      title: '내가쓴 글 글제목3',
+      liked_count: '3',
+      comment_count: '3',
+    },
+  ];
   return (
     <>
-      <div>
+      <Styled.MyArticlesDiv>
         {articles.map((article, idx) => (
-          <div key={idx}>{article}</div>
+          <Styled.MyArticleDiv key={idx} article={article}>
+            <span className="article_board">{article.board}</span>
+            <span className="article_title">{article.title}</span>
+            <div className="favorite_icon">
+              <FavoriteBorder />
+            </div>
+            <div className="comment_icon">
+              <SmsOutlined />
+            </div>
+          </Styled.MyArticleDiv>
         ))}
-      </div>
+      </Styled.MyArticlesDiv>
     </>
   );
 };
