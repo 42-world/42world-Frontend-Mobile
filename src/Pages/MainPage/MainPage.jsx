326051a79fa3db3f8675f99bb411d7719d520514
GulSam00 <gulsamcono@gmail.com>
2022-01-22 17:59:46 +0900

    Feat : 302 Error

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import { Body } from './Components';
 import { Header } from '../../Components';
-
+import AuthService from '../../Network/AuthService';
 import Styled from './MainPage.styled';
 
 const MainPage = () => {
+  useEffect(async () => {
+    const result = await AuthService.github();
+  }, []);
   return (
     <>
       <Styled.MainHeader>
