191be81a1421e21202a6ca6e66e310591b971aa8
rkskekzzz <wkdlfflxh@naver.com>
2022-01-28 16:01:06 +0900

    Feat : EditArticlePage 추가

--- src/Network/ArticleService.js
+++ src/Network/ArticleService.js
@@ -114,35 +114,35 @@ const ArticleService = {
   /**
    * **GET** One Articles By Articles ID
    * @param {string} articlesId
-   * @returns {{ \
-   *    id: number, \
-   *    title: string, \
-   *    content: string, \
-   *    viewCount: number, \
-   *    categoryId: number, \
-   *    category: { \
-   *        id: number, \ \
-   *        name: string, \
-   *        createdAt: Date, \
+   * @returns {Promise<{
+   *    id: number,
+   *    title: string,
+   *    content: string,
+   *    viewCount: number,
+   *    categoryId: number,
+   *    category: {
+   *        id: number,
+   *        name: string,
+   *        createdAt: Date,
    *        updatedAt: Date
-   *    }, \
-   *    writerId: number, \
-   *    writer: { \
-   *        id: number, \
-   *        nickname: string, \
-   *        oauthToken: string, \
-   *        isAuthenticated: boolean, \
-   *        lastLogin: Date, \
-   *        role: CADET, \
-   *        character: number, \
-   *        deletedAt: Date, \
-   *        createdAt: Date, \
-   *        updatedAt: Date \
-   *    }, \
-   *    deletedAt: Date, \
-   *    createdAt: Date, \
-   *    updatedAt: Date \
-   * }} articles \
+   *    },
+   *    writerId: number,
+   *    writer: {
+   *        id: number,
+   *        nickname: string,
+   *        oauthToken: string,
+   *        isAuthenticated: boolean,
+   *        lastLogin: Date,
+   *        role: CADET,
+   *        character: number,
+   *        deletedAt: Date,
+   *        createdAt: Date,
+   *        updatedAt: Date
+   *    },
+   *    deletedAt: Date,
+   *    createdAt: Date,
+   *    updatedAt: Date
+   * }>} articles \
    * `200` : success \
    * `401` : fail
    */
