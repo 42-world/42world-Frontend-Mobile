bfb2324148967bde5fc1b9632d24db6d2e8c1cd5
PIut0 <slimdoks@gmail.com>
2022-01-29 23:14:11 +0900

    Feat: Footer 스타일 적용

--- src/Components/Footer.styled.js
+++ src/Components/Footer.styled.js
@@ -1,9 +1,26 @@
 import styled from 'styled-components';
+import GlobalStyled from '../Styled/Global.styled';
 
 const Footer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  padding: 0.5rem;
+  margin-top: auto;
+  div {
+    margin-top: auto;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 0.2rem;
+
+    img {
+      width: 1.5rem;
+      margin: 0.3rem;
+    }
+  }
 `;
+
 const Styled = { Footer };
 
 export default Styled;
