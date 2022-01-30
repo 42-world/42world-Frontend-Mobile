d0fcc7fa8b7faefa46b18dfae2cbe5ab2c8202b4
hyongti <hyongtiii@gmail.com>
2022-01-30 11:30:12 +0900

    Feat : 이름 옆에 안녕하세요 추가

--- src/Pages/ProfilePage/Components/Info.styled.js
+++ src/Pages/ProfilePage/Components/Info.styled.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import GlobalStyled from 'Styled/Global.styled';
 
-const InfoBox = styled.div`
+const InfoDiv = styled.div`
   width: 90%;
   display: flex;
   justify-content: space-between;
@@ -8,29 +9,26 @@ const InfoBox = styled.div`
   padding: 10px;
 
   color: white;
+
+  .middle {
+    display: flex;
+    align-items: flex-end;
+    .name {
+      font-style: normal;
+      font-weight: 900;
+      font-size: 1.2rem;
+      line-height: 20px;
+
+      color: ${GlobalStyled.theme.textColorWhite};
+    }
+    .welcome-message {
+      padding-left: 0.4rem;
+      font-size: 0.5rem;
+      color: ${GlobalStyled.theme.textColorLightGray};
+    }
+  }
 `;
 
-const NameDiv = styled.div`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 20px;
-  line-height: 20px;
-
-  color: #ffffff;
-`;
-
-const EmailDiv = styled.div`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 13px;
-  line-height: 20px;
-  /* identical to box height, or 154% */
-
-  color: #979797;
-`;
-
-const Styled = { NameDiv, EmailDiv, InfoBox };
+const Styled = { InfoDiv };
 
 export default Styled;
