5028a59ebc16ae5d411c2faaf7ecacd5ad10ffd2
hyongti <hyongtiii@gmail.com>
2022-02-06 23:46:48 +0900

    Fix : 클릭 시 조회수 2번 올라가는 오류 수정

--- src/Pages/ArticlePage/ArticlePage.jsx
+++ src/Pages/ArticlePage/ArticlePage.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Body, CommentContainer } from './Components';
-import { Header } from '../../Components';
+import { Body } from './Components';
+import { Header } from 'Components';
 
 import Styled from './ArticlePage.styled';
-import ArticleService from 'Network/ArticleService';
 const ArticlePage = () => {
   const { id } = useParams();
-  const [categoryId, setCategoryId] = useState(0);
-  const getCate = async () => {
-    const result = await ArticleService.getArticlesById(id);
-    setCategoryId(result.categoryId);
-  };
-  useEffect(() => {
-    getCate();
-  }, []);
 
   return (
     <>
       <Header />
       <Styled.ArticlePageDiv>
-        <Body articleId={id} categoryId={categoryId} />
-        {categoryId !== 3 && <CommentContainer articleId={id} />}
+        <Body articleId={id} />
+        {/* {categoryId !== 3 && <CommentContainer articleId={id} />} */}
       </Styled.ArticlePageDiv>
     </>
   );
